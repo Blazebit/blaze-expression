35e2f029fe5a43aa614d7a70040b0563286662e1
Christian Beikov <christian.beikov@gmail.com>
2024-04-10 15:13:17 +0200

    Update license headers

--- declarative/impl/src/main/java/com/blazebit/expression/declarative/impl/TypeAdapterDeclarativeFunctionMetadataProcessor.java
+++ declarative/impl/src/main/java/com/blazebit/expression/declarative/impl/TypeAdapterDeclarativeFunctionMetadataProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 - 2022 Blazebit.
+ * Copyright 2019 - 2024 Blazebit.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
